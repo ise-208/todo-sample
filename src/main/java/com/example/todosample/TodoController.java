4eaf3befd408587236fc3b4c261cd420f24f2620
ise-208 <seed.0ri00@gmail.com>
2023-12-10 22:03:14 +0900

    DBのレコードを表示させる

--- src/main/java/com/example/todosample/TodoController.java
+++ src/main/java/com/example/todosample/TodoController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Collection;
+import java.util.List;
+
 @Controller
 public class TodoController {
     private final TodoRepository todoRepository;
@@ -16,11 +19,13 @@ public class TodoController {
 
     @GetMapping("/home")
     public String home(Model model) {
+        Collection<TodoEntity> todos = todoRepository.findAll();
+        model.addAttribute("todos",todos);
         model.addAttribute("todoEntity", new TodoEntity());
         return "home";
     }
 
-    @PostMapping("/api/home")
+    @PostMapping("/home")
     public String addTodo(@ModelAttribute TodoEntity todoEntity) {
         todoRepository.insert(todoEntity);
         return "redirect:/home";
